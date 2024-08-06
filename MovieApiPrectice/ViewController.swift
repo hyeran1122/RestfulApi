3872cd3fe59696e045961c4b46b460587840d93a
hyeran1122 <97385586+hyeran1122@users.noreply.github.com>
2024-08-06 22:21:30 +0900

    Initial Commit

---
+++ MovieApiPrectice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApiPrectice
+//
+//  Created by 김혜란 on 8/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
